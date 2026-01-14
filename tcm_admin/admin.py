0764488c2bb1bfabaec4185eec951ffca08967da
Kakhi Mtchedluri <kmchedluri@gmail.com>
2026-01-14 04:44:00 +0400

    more small

--- tcm_admin/admin.py
+++ tcm_admin/admin.py
@@ -75,7 +75,7 @@ class AnalysisAdmin(admin.ModelAdmin):
     # Helper features
     search_fields = ('blood_test', 'blood_test_full', 'panel')
     list_filter = ('panel', 'vital_marker', 'severity')
-    list_per_page = 20
+    list_per_page = 10
     show_full_result_count = False
 
 
