ac6cb31ffacacd7fc5aa98b3e04e7de4065877d9
Kakhi Mtchedluri <kmchedluri@gmail.com>
2026-02-13 17:52:20 +0400

    addign migrate

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+echo "Running migrations..."
+python manage.py migrate --noinput
 python manage.py collectstatic --no-input --clear
 
 # Try to create superuser. If it exists, skip without error.
